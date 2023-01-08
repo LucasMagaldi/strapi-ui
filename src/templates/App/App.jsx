4d2898efea5a4e8f943b504fe791b6d149b31008
LucasMagaldi <lucas.magaldi@hotmail.com>
2023-01-08 20:31:15 -0300

    styled componentss

--- src/templates/App/App.jsx
+++ src/templates/App/App.jsx
@@ -1,5 +1,13 @@
+import * as Style from "./Style";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Style.Wrapper background="blue">
+        <h1>hello</h1>
+      </Style.Wrapper>
+    </div>
+  );
 }
 
 export default App;
